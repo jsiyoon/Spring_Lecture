5fd9ef1205bafa605213f9c995deb5004f1e3713
Jsiyoon <jjc9090@naver.com>
2021-12-02 17:12:03 +0900

    lombok사용법-@Data, @AllArgsConstructor, @NoArgsConstructor

--- src/main/java/lecture/p01lombok/Supplier.java
+++ src/main/java/lecture/p01lombok/Supplier.java
@@ -4,10 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
+@Data //getter, setter, toString 등 포함
 @AllArgsConstructor //생성자
 @NoArgsConstructor //파라미터 없는 생성자
 public class Supplier {
+	
 	private int id;
 	private String name;
 	private String address;
