3f87b0c1bbbfde9fa41db4489fc469b1de7b8a08
Jsiyoon <jjc9090@naver.com>
2021-12-06 17:15:25 +0900

    Controller04 - met09

---
+++ src/main/java/org/zerock/controller/p01Controller/bean/Bean06.java
@@ -0,0 +1,12 @@
+package org.zerock.controller.p01Controller.bean;
+
+import java.util.ArrayList;
+
+import lombok.Data;
+
+@Data
+public class Bean06 {
+	private String name;
+	private int id;
+	private ArrayList<String> address;
+}
