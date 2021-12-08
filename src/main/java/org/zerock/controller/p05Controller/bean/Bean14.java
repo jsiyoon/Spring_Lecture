689f1621c1261e401b34cbbecc367ad1dab09c29
Jsiyoon <jjc9090@naver.com>
2021-12-08 10:38:16 +0900

    Controller09 - /met14

---
+++ src/main/java/org/zerock/controller/p05Controller/bean/Bean14.java
@@ -0,0 +1,14 @@
+package org.zerock.controller.p05Controller.bean;
+
+import lombok.Data;
+
+@Data
+public class Bean14 {
+	private Integer customerID;
+	private String customerName;
+	private String contactName;
+	private String address;
+	private String city;
+	private String postalCode;
+	private String country;
+}
