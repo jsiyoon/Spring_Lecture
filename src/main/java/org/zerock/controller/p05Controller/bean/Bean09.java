6a323b02a4f8e81a72388903bc28e533285ab96e
Jsiyoon <jjc9090@naver.com>
2021-12-08 10:37:28 +0900

    Controller09 - /met09

---
+++ src/main/java/org/zerock/controller/p05Controller/bean/Bean09.java
@@ -0,0 +1,10 @@
+package org.zerock.controller.p05Controller.bean;
+
+import lombok.Data;
+
+@Data
+public class Bean09 {
+	private Integer employeeID;
+	private String lastName;
+	private String firstName;
+}
