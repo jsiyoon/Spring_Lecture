f3b3b7d8be6abc28f5a6b66439e0e67a0a4aa7d9
Jsiyoon <jjc9090@naver.com>
2021-12-08 14:51:21 +0900

    bean17(productName, categoryName, unit, price)

---
+++ src/main/java/org/zerock/controller/p05Controller/bean/Bean17.java
@@ -0,0 +1,11 @@
+package org.zerock.controller.p05Controller.bean;
+
+import lombok.Data;
+
+@Data
+public class Bean17 {
+	private String productName;
+	private String categoryName;
+	private String unit;
+	private double price;
+}
