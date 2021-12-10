5788fdce722812921be86cc37eefb53025cfa3a2
Jsiyoon <jjc9090@naver.com>
2021-12-10 12:18:20 +0900

    테이블만들어 isnert, update, delete

--- src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
+++ src/test/java/org/zerock/mapper/project1/BoardMapperTest.java
@@ -1,7 +1,12 @@
 package org.zerock.mapper.project1;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,11 +22,13 @@ public class BoardMapperTest {
 	@Autowired
 	public BoardMapper mapper;
 	
+	/*assert : 값이 조건을 만족하는지 아닌지 check*/
 	@Test
 	public void mapperTest() {
 		assertNotNull(mapper);
 	}
 	
+	//insert
 	@Test
 	public void insertTest() {
 		BoardVO vo = new BoardVO();
@@ -34,4 +41,98 @@ public class BoardMapperTest {
 		assertEquals(1, cnt);
 	}
 
+	//insert&Select
+	@Test
+	public void insertAndSelectTest() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("test용 제목" + Math.random());
+		vo.setContent("테스트 용 본문" + Math.random());
+		vo.setWriter("tester");
+		
+		mapper.insert(vo);
+		
+		assertNotNull(vo.getId());
+		assertNotEquals(0, vo.getId().intValue());
+		
+		BoardVO lastInserted = mapper.read(vo.getId());
+		
+		assertEquals(vo.getTitle(), lastInserted.getTitle());
+		assertEquals(vo.getContent(), lastInserted.getContent());
+		assertEquals(vo.getWriter(), lastInserted.getWriter());
+	}
+	
+	//update
+	@Test
+	public void updateTest() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("test용 제목" + Math.random());
+		vo.setContent("테스트 용 본문" + Math.random());
+		vo.setWriter("tester");
+		
+		mapper.insert(vo);
+		
+		assertNotNull(vo.getId());
+		assertNotEquals(0, vo.getId().intValue());
+		
+		String newTitle = "update용 제목" + Math.random();
+		String newContent = "update용 본문" + Math.random();
+		
+		vo.setTitle(newTitle);
+		vo.setContent(newContent);
+		
+		mapper.update(vo);
+		
+		BoardVO updateVO = mapper.read(vo.getId());
+		
+		assertEquals(newTitle, updateVO.getTitle());
+		assertEquals(newContent, vo.getContent());
+	}
+	
+	//insert하자마자 Delete
+	@Test
+	public void insertAndDeleteTest() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("삭제용 제목");
+		vo.setContent("삭제용 본문");
+		vo.setWriter("tester");
+		
+		int cnt = mapper.insert(vo);
+		
+		assertEquals(1,cnt);
+		
+		cnt = mapper.delete(vo.getId());
+		
+		assertEquals(1, cnt);
+		
+		BoardVO deleted = mapper.read(vo.getId());
+		assertNull(deleted);
+	}
+	
+	//list테스트
+	@Test
+	public void getListTest() {
+		List<BoardVO> list = mapper.getList();
+		assertNotNull(list);
+		assertTrue(list.size() > 0);
+		
+		BoardVO vo = new BoardVO();
+		vo.setTitle("list 테스트용");
+		vo.setContent("list 테스트용 본문");
+		vo.setWriter("tester");
+		
+		mapper.insert(vo);
+		
+		List<BoardVO> list2 = mapper.getList();
+		
+		assertEquals(list.size() + 1, list2.size());
+		
+		for(BoardVO item : list2) {
+			assertNotNull(item.getId());
+			assertNotNull(item.getTitle());
+			assertNotNull(item.getContent());
+			assertNotNull(item.getWriter());
+			assertNotNull(item.getInserted());
+			assertNotNull(item.getUpdated());
+		}
+	}
 }
