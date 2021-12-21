b78e31511634cd977d18c48348171325d0ecccca
Jsiyoon <jjc9090@naver.com>
2021-12-22 01:13:59 +0900

    Board테이블 service

--- src/main/java/org/zerock/service/project1/BoardService.java
+++ src/main/java/org/zerock/service/project1/BoardService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.zerock.domain.project1.BoardVO;
+import org.zerock.domain.project1.PageInfoVO;
 import org.zerock.mapper.project1.BoardMapper;
 
 import lombok.Setter;
@@ -40,4 +41,46 @@ public class BoardService {
 	public List<BoardVO> getList(){
 		return mapper.getList();
 	}
+
+	public List<BoardVO> getListPage(Integer page, Integer numberPerPage) {
+		//sql에서 사용할 record 시작번호(0-index)
+		Integer from = (page-1) * 10;
+		return mapper.getListPage(from, numberPerPage);
+	}
+
+	public PageInfoVO getPageInfo(Integer page, Integer numberPerPage) {
+		//총 게시불 수
+		Integer countRows = mapper.getCountRows();
+		
+		//마지막 페이지 번호
+		Integer lastPage = (countRows-1) / numberPerPage + 1;
+		
+		//페이지네이션 가장 왼쪽 번호
+		Integer leftPageNumber = (page-1) / 10 * 10 + 1;
+		
+		//페이지네이션 가장 오른쪽 번호
+		Integer rightPageNumber = (page-1) / 10 * 10 + 10;
+		
+		//가장 마지막 페이지 넘어가지 않도록
+		rightPageNumber = rightPageNumber > lastPage ? lastPage : rightPageNumber;
+		
+		//이전 페이지 버튼 존재 유무
+		Boolean hasPrevButton = leftPageNumber != 1;
+		
+		//다음 페이지 버튼 존재 유무
+		Boolean hasNextButton = rightPageNumber != lastPage;
+		
+		PageInfoVO pageInfo = new PageInfoVO();
+		
+		pageInfo.setLastPage(lastPage);
+		pageInfo.setCountRows(countRows);
+		pageInfo.setCurrentPage(page);
+		pageInfo.setLeftPageNumber(leftPageNumber);
+		pageInfo.setRightPageNumber(rightPageNumber);
+		pageInfo.setHasPrevButton(hasPrevButton);
+		pageInfo.setHasNextButton(hasNextButton);
+		
+		return pageInfo;
+	}
+
 }
