80c286b99ee55fc551708e6e702bd01f2b3c7681
Jsiyoon <jjc9090@naver.com>
2021-12-22 21:51:31 +0900

    Reply테이블 메소드들

--- src/main/java/org/zerock/mapper/project1/ReplyMapper.java
+++ src/main/java/org/zerock/mapper/project1/ReplyMapper.java
@@ -8,6 +8,16 @@ public interface ReplyMapper {
 	public List<ReplyVO> list(Integer boardId);
 
 	public int insert(ReplyVO reply);
+
+	public ReplyVO selectById(Integer id);
+
+	public int update(ReplyVO newReply);
+
+	public int delete(Integer id);
+
+	public void deleteByBoardId(Integer boardid);
+
+	public int deleteByMemberId(String memberId);
 	
 	
 }
