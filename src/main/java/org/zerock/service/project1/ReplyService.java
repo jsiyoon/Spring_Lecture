b52267b9814094c695d97730816232812c876115
Jsiyoon <jjc9090@naver.com>
2021-12-22 21:50:55 +0900

    Reply테이블 service

--- src/main/java/org/zerock/service/project1/ReplyService.java
+++ src/main/java/org/zerock/service/project1/ReplyService.java
@@ -23,4 +23,17 @@ public class ReplyService {
 
 		return mapper.insert(reply) == 1;
 	}
+
+	public ReplyVO readById(Integer id) {
+		return mapper.selectById(id);
+	}
+
+	public boolean update(ReplyVO newReply) {
+		return mapper.update(newReply) == 1;
+	}
+
+	public boolean delete(Integer id) {
+		return mapper.delete(id) == 1;
+	}
+
 }
